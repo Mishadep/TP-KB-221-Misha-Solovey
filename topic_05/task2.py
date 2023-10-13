b3057b25a0b6d472a9c95cb0bbfb2038dce3e49e
Misha <soloveyog45@gmail.com>
2023-10-13 22:07:16 +0300

    fix code

--- topic_05/task2.py
+++ topic_05/task2.py
@@ -3,8 +3,10 @@ import requests
 def get_exchange_rate(currency_code):
     url = f"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode={currency_code}&json"
     response = requests.get(url)
+    response.raise_for_status()  # Проверка статуса ответа
+
     data = response.json()
-    if data:
+    if data and isinstance(data, list) and len(data) > 0:
         return data[0]["rate"]
     else:
         raise Exception("Помилка отримання курсу валюти")
@@ -14,6 +16,10 @@ def convert_currency(amount, currency_code):
         rate = get_exchange_rate(currency_code)
         converted_amount = amount * rate
         return converted_amount
+    except requests.exceptions.RequestException as e:
+        return f"Помилка: Немає з'єднання з сервером. Перевірте підключення до Інтернету."
+    except (KeyError, IndexError):
+        return f"Помилка: Невірний формат відповіді сервера."
     except Exception as e:
         return f"Помилка: {e}"
 
